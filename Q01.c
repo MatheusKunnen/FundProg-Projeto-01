9c52cc5618f6afa7da911b44525ae191a3c1faa1
Matheus Kunnen Ledesma <matheus@Matheuss-MacBook-Pro.local>
2019-04-20 09:44:49 -0300

    Analise da Questao 01

---
+++ Q01.c
@@ -0,0 +1,56 @@
+# include<stdio.h>
+/* 
+    Projeto-01 | Fund. Prog 01 | Turma Eng. Comp. 2019/01
+    Alunos: Matheus Kunnen
+            Felipe Rua
+            Jhonny Assis
+    Questao - 01:
+                Construa um programa que produza uma folha de calend ́ario para um ano completo, 
+                o qual deve ser informado pelo usu ́ario via teclado! O programa a ser desenvolvido 
+                deve levar em conta que o mˆes de fevereiro pode ter 29 dias (ano bissexto).
+*/
+
+// Calcula primeiro dia do mes
+int diaInicioMes(int mes, int ano);
+// Retorna 1 se o ano e bissexto e 0 no caso contrario
+int ehBissexto(int ano);
+// Imprime a cabecera do mes
+void imprimeCabMes(int mes);
+// Imprime dias do mes
+void imprimeDiasMes(int mes, int ano);
+// Retorna o total de dias do mes
+int totalMes(int mes, int ano);
+
+int main(){
+    return 0;
+}
+
+// Calcula primeiro dia do mes
+int diaInicioMes(int mes, int ano){
+
+}
+
+// Retorna 1 se o ano e bissexto e 0 no caso contrario
+int ehBissexto(int ano){
+
+}
+
+// Imprime a cabecera do mes
+void imprimeCabMes(int mes){
+
+}
+
+// Imprime dias do mes
+void imprimeDiasMes(int mes, int ano){
+
+}
+
+// Retorna o total de dias do mes
+int totalMes(int mes, int ano){
+    return 0;
+}
+
+/*
+    Bibliografia:
+            * Calculo primeiro dia do mes: https://es.wikibooks.org/wiki/Algoritmia/Algoritmo_para_calcular_el_d%C3%ADa_de_la_semana
+*/
