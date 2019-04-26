0059574c8a6604c97197e12fd9c8472341c5494c
Matheus Kunnen Ledesma <matheus@Matheuss-MacBook-Pro.local>
2019-04-26 13:45:11 -0300

    Q02

--- Q02.c
+++ Q02.c
@@ -1,21 +1,25 @@
 # include<stdio.h>
-/* 
+/*
     Projeto-01 | Fund. Prog 01 | Turma Eng. Comp. 2019/01
     Alunos: Matheus Kunnen
             Jhonny Assis
     Questao - 02:
-                Construa um programa que produza uma folha de calend ́ario para um ano completo, 
-                o qual deve ser informado pelo usuario via teclado! O programa a ser desenvolvido 
+                Construa um programa que produza uma folha de calend ́ario para um ano completo,
+                o qual deve ser informado pelo usuario via teclado! O programa a ser desenvolvido
                 deve levar em conta que o mˆes de fevereiro pode ter 29 dias (ano bissexto).
 */
-#define SIZE (1 << 5) 
+#define SIZE (1 << 4)
 int main (){
         int x, y, i;
-        for (y = SIZE - 1; y >= 0; y--, putchar('\n')) {
-                for (i = 0; i < y; i++) 
-                        putchar(' ');
+        printf("\tX\t\tY\t\tI\t\t\n");
+        for (y = SIZE - 1; y >= 0; y--, putchar(/*'\n'*/' ')) {
+                printf("%10d\t%10d\t%10d\n", x, y, i);
+                for (i = 0; i < y; i++)
+                    printf("%10d\t%10d\t%10d\n", x, y, i);
+                        //putchar(' ');
                 for (x = 0; x + y < SIZE; x++)
-                        printf((x & y)? " " : "* "); 
+                    printf("%10d\t%10d\t%10d\n", x, y, i);
+                        //printf((x & y)? " " : "* ");
         }
-        return 0; 
+        return 0;
 }
