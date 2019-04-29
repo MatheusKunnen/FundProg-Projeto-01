809cfdfc9f48acba4fb80585e06b77af9504766f
Matheus Kunnen Ledesma <matheus@Matheuss-MacBook-Pro.local>
2019-04-28 21:10:18 -0300

    Optimizacion Q04

--- Q03.c
+++ Q03.c
@@ -1,18 +1,129 @@
-# include<stdio.h>
-/* 
+#include <stdio.h>
+/*
     Projeto-01 | Fund. Prog 01 | Turma Eng. Comp. 2019/01
     Alunos: Matheus Kunnen
-            Felipe Rua
             Jhonny Assis
     Questao - 01:
-                A proposta  ́e transmitir esses sımbolos usando o codigo ASCII - por exemplo, ao inves de transmitir ’A’, 
-                e transmitido o valor 65. Anna e Bob observaram que, para a faixa de valores utilizada para as letras, 
-                os valores ocupam apenas um byte e que o seu bit mais significativo nao precisa ser usado. Por exemplo, 
+                A proposta  ́e transmitir esses sımbolos usando o codigo ASCII - por exemplo, ao inves de transmitir ’A’,
+                e transmitido o valor 65. Anna e Bob observaram que, para a faixa de valores utilizada para as letras,
+                os valores ocupam apenas um byte e que o seu bit mais significativo nao precisa ser usado. Por exemplo,
                 o valor 65 em bin ́ario  ́e 01000001 - note que o bit mais a esquerda e zero. Eles decidiram explorar este bit
-                para determinar se a letra transmitida foi corrompida ou nao. A proposta consiste em alterar este bit mais 
+                para determinar se a letra transmitida foi corrompida ou nao. A proposta consiste em alterar este bit mais
                 significativo de tal forma que a soma dos bits seja sempre par (2, 4, 6 or 8)
 */
+void codificaMensagem();
+int codificarLetra(char letra);
+void decodificaMensagem();
+char decodificarLetra(int letra);
+int somaBinario(int dec);
 
-int main(){
-    return 0;
+int main()
+{
+        int men = -1;
+        while (men != 0)
+        {
+                printf("1 - Codificar Mensagem\n2 - Decodificar Mensagem\n0 - SAIR\n>", " ");
+                scanf("%d", &men);
+                switch (men)
+                {
+                case 1:
+                        codificaMensagem();
+                        break;
+                case 2:
+                        decodificaMensagem();
+                        break;
+                case 0:
+                        break;
+                default:
+                        men = 0;
+                }
+        }
+        return 0;
+}
+// Ler os caracgteres
+void codificaMensagem()
+{
+        char letra;
+        printf("Digite a Mensagem:\n");
+        scanf("%c ", &letra);
+        printf("Mensagem Codificada:\n ");
+        do
+        {
+                scanf("%c", &letra);
+                printf("%d ", codificarLetra(letra));
+        } while (letra != 46);
+        printf("\n\n-------------------------------------------------------------\n\n");
+}
+
+int codificarLetra(char letra)
+{
+        int soma = 0;
+        soma = somaBinario(letra);
+        if (soma % 2 != 0)
+        {
+                return letra + 128;
+        }
+        else
+        {
+                return letra;
+        }
+}
+
+void decodificaMensagem()
+{
+        char letra;
+        int lDec = 0, dec;
+        printf("Digite a Mensagem Codificada:\n");
+        scanf("%c ", &letra);
+        printf("Mensagem decodificada:\n ");
+        while (lDec != 46)
+        {
+                //printf("while %c\n", letra);
+                lDec = 0;
+                while (letra != 32)
+                {
+                        if (letra >= 48 &&letra <= 57)
+                        {
+                                lDec = lDec * 10 + (letra - 48);
+                        } else if (lDec == 46) break;
+                        scanf("%c", &letra);
+                }
+                printf("%c", decodificarLetra(lDec));
+                scanf("%c", &letra);
+        }
+        printf("\n\n-------------------------------------------------------------\n\n");
+}
+
+char decodificarLetra(int letra)
+{
+        int soma = 0;
+        soma = somaBinario(letra);
+        if (soma % 2 != 0)
+        {
+                return '*';
+        }
+        else if (letra > 127)
+        {
+                return (char) letra - 128;
+        }
+        else
+        {
+                return (char) letra;
+        }
+}
+
+int somaBinario(int dec)
+{
+        int soma = 0, aux = 1;
+        while (dec > 0)
+        {
+                if (dec % 2 != 0)
+                {
+                        soma++;
+                        //*bin += aux;
+                }
+                aux *= 10;
+                dec /= 2;
+        }
+        return soma;
 }
--- Q04.c
+++ Q04.c
@@ -1,9 +1,9 @@
-/* 
+/*
     Projeto-01 | Fund. Prog 01 | Turma Eng. Comp. 2019/01
     Alunos: Matheus Kunnen
             Jhonny Assis
-    Questao - 04: Fac a um programa que, dado um numero, identifique a operadora e verifique a validade do nu ́mero. As situa ̧c ̃oes a serem tratadas e que devem ser indicadas por mensagens adequadas sa ̃o as seguintes:
-                        1. Nu ́meros que contiverem caracteres n ̃ao num ́ericos (caracteres fora do intervalo [0;9] ); 
+    Questao - 04: Faca um programa que, dado um numero, identifique a imprimirOperadora e verifique a validade do nu ́mero. As situa ̧c ̃oes a serem tratadas e que devem ser indicadas por mensagens adequadas sa ̃o as seguintes:
+                        1. Nu ́meros que contiverem caracteres n ̃ao num ́ericos (caracteres fora do intervalo [0;9] );
                         2. Nu ́meros com tamanho incorreto;
                         3. Nu ́meros de operadoras desconhecidas (que nao precisam ser validados);
                         4. Nu ́meros de operadoras conhecidas, mas invalidos;
@@ -13,149 +13,304 @@
 #include <stdio.h>
 #include <math.h>
 
-int ver_operadora(unsigned long long numero);
-void numero_invalido(unsigned long long numero);
-void operadora(int x);
-int numero_resultante(unsigned long long numero);
+int lerNroCartao(unsigned long long *numero, int *tamanho);
+int verOperadora(unsigned long long numero, int *codOperadora);
+void numeroInvalido(unsigned long long numero, int codOperadora, int tamanho);
+void imprimirOperadora(int codOperadora);
+int validarNro(unsigned long long numero, int codOperadora, int tamanho);
 unsigned long long restoLong(unsigned long long d1, unsigned long long d2);
+
 int main()
 {
-    int prefixo, x, y;
-    unsigned long long numero;
-    printf("Digite o numero do Cartao: ");
-    scanf("%llu", &numero);
-    x = ver_operadora(numero);
-    //
-    if (x == 0) //NUMERO INVALIDO, CASO 1,2,3
-    {
-        numero_invalido(numero);
-    }
-    else
-    {
-        y = numero_resultante(numero);
-        if(y % 10 != 0) //CASO 4
-            printf("Numero de operadora conhecida, mas invalido ");
-        else //CASO 5
+        int codOperadora, tamanho;
+        unsigned long long numero;
+        //printf("Digite o numero do Cartao: ");
+        //scanf("%llu", &numero);
+        //codOperadora = verOperadora(numero);
+        if (lerNroCartao(&numero, &tamanho))
         {
-            printf("\nNumero valido\nOperadora: ");
-            operadora(x);
-        }
-    }
-    return 0;
-}
-
-int ver_operadora(unsigned long long numero)
-{
-    int x = 0;
-    if (numero / (unsigned long long) (pow(10, 14)) == 51 || numero / (unsigned long long) (pow(10, 14)) == 55)
-    {
-        x = 1; //Mastercard
-    }
-    else if (numero / (unsigned long long) (pow(10, 12)) == 4 || numero / (unsigned long long) (pow(10, 15)) == 4)
-    {
-        x = 2; //Visa
-    }
-    else if (numero / (unsigned long long) (pow(10, 13)) == 34 || numero / (unsigned long long) (pow(10, 13)) == 37)
-    {
-        x = 3; //Amex
-    }
-    else if (numero / (unsigned long long) (pow(10, 12)) == 30 || numero / (unsigned long long) (pow(10, 12)) == 36 || numero / (unsigned long long) (pow(10, 12)) == 38)
-    {
-        x = 4; //Dinnes
-    }
-    else if (numero / (unsigned long long) (pow(10, 12)) == 6011)
-    {
-        x = 5; //Discover
-    }
-    else if (numero / (unsigned long long) (pow(10, 11)) == 2014 || numero / (unsigned long long) (pow(10, 11)) == 2149)
-    {
-        x = 6; //enRoute
-    }
-    else if (numero / (unsigned long long) (pow(10, 15)) == 3)
-    {
-        x = 7; //JCB
-    }
-    else if (numero / (unsigned long long) (pow(10, 11)) == 2131 || numero / (unsigned long long) (pow(10, 11)) == 1800)
-    {
-        x = 8; //JCB
-    }
-    //SE X=0, O NUMERO � INVALIDO.
-    return x;
-}
-
-void numero_invalido(unsigned long long numero)
-{
-        printf("Entro em numero_invalido\n");
-    if (((numero / (unsigned long long) (pow(10, 12))) < 10) || ((numero / (unsigned long long) (pow(10, 16))) > 10))
-    {
-        printf("Tamanho incorreto "); //CASO 2
-    }
-    else if (((numero / (unsigned long long) (pow(10, 12))) > 10) && ((numero / (unsigned long long) (pow(10, 16))) < 10))
-    {
-        printf("numero de operadoras desconhecidas  "); //CASO 3
-    }
-}
-void operadora(int x)
-{
-    switch (x)
-    {
-    case 1:
-        printf("Mastercard\n");
-        break;
-    case 2:
-        printf("Visa\n");
-        break;
-    case 3:
-        printf("Amex\n");
-        break;
-    case 4:
-        printf("Diners\n");
-        break;
-    case 5:
-        printf("Discover\n");
-        break;
-    case 6:
-        printf("enRoute\n");
-        break;
-    case 7:
-        printf("JCB\n");
-        break;
-    case 8:
-        printf("JCB\n");
-        break;
-    }
-}
-int numero_resultante(unsigned long long numero)
-{
-    //printf("Entro em resultante\n"); //TESTE
-    int i = 0, j = 0, numero_resultante = 0, num;
-    unsigned long long int aux_numero, s, r;
-    aux_numero = numero * 10;
-    while (aux_numero > 0) //Quantos algarismos tem o numero
-    {
-        num = aux_numero % 10;
-        //printf("I: %d N: %d\n", i, num); // TESTE
-        if (i % 2 == 0){
-                num = num * 2;
-                if (num >= 10){
-                    num = num % 10 + num / 10;
+                if (verOperadora(numero, &codOperadora))
+                {
+                        if (validarNro(numero, codOperadora, tamanho))
+                        {
+                                printf("Estado: \x1b[32mValido.\x1b[0m\n");
+                        }
+                        else
+                        {
+                                printf("Estado: Invalido. ");
+                                numeroInvalido(numero, codOperadora, tamanho);
+                        }
                 }
         }
-        numero_resultante += num;
-        aux_numero /= 10;
-        i++;
-    }
-    //printf("R: %d\n", numero_resultante); //TESTE
-    return numero_resultante;
+        /*if (codOperadora == 0) //NUMERO INVALIDO, CASO 1,2,3
+        {
+                numeroInvalido(numero);
+        }
+        else
+        {
+                if (validarNro(numero)) //CASO 4
+                        printf("Numero de imprimirOperadora conhecida, mas invalido ");
+                else //CASO 5
+                {
+                        printf("\nNumero valido\nOperadora: ");
+                        imprimirOperadora(codOperadora);
+                }
+        }*/
+        return 0;
+}
+
+int verOperadora(unsigned long long numero, int *codOperadora)
+{
+        *codOperadora = 0;
+        while (numero / 10000 > 0)
+        {
+                numero /= 10;
+        }
+        printf("Operadora: ");
+        switch (numero / 1000)
+        {
+        case 1:
+                if (numero == 1800)
+                {
+                        printf("JCB");
+                        *codOperadora = 8; // JCB de tamanho 15
+                }
+                break;
+        case 2:
+                switch (numero)
+                {
+                case 2014:
+                case 2149:
+                        printf("enRoute");
+                        *codOperadora = 6;
+                        break;
+                case 2131:
+                        printf("JCB");
+                        *codOperadora = 8; // JCB de tamanho 15
+                        break;
+                }
+                break;
+        case 3:
+        case 5:
+                switch (numero / 100)
+                {
+                case 30:
+                case 36:
+                case 38:
+                        printf("Diners");
+                        *codOperadora = 4;
+                        break;
+                case 34:
+                case 37:
+                        printf("Amex");
+                        *codOperadora = 3;
+                case 51:
+                case 55:
+                        printf("Mastercard");
+                        *codOperadora = 1;
+                        break;
+                default:
+                        printf("JCB");
+                        *codOperadora = 7; // JCB de tamanho 16
+                        break;
+                }
+                break;
+        case 4:
+                printf("Visa");
+                *codOperadora = 2;
+                break;
+        case 6:
+                if (numero == 6011)
+                {
+                        printf("Discover");
+                        *codOperadora = 5;
+                }
+                break;
+        default:
+                printf("Desconhecida\n");
+        }
+        printf("\n");
+        /*int codOperadora = 0;
+        if (numero / (unsigned long long)(pow(10, 14)) == 51 || numero / (unsigned long long)(pow(10, 14)) == 55)
+        {
+                codOperadora = 1; //Mastercard
+        }
+        else if (numero / (unsigned long long)(pow(10, 12)) == 4 || numero / (unsigned long long)(pow(10, 15)) == 4)
+        {
+                codOperadora = 2; //Visa
+        }
+        else if (numero / (unsigned long long)(pow(10, 13)) == 34 || numero / (unsigned long long)(pow(10, 13)) == 37)
+        {
+                codOperadora = 3; //Amex
+        }
+        else if (numero / (unsigned long long)(pow(10, 12)) == 30 || numero / (unsigned long long)(pow(10, 12)) == 36 || numero / (unsigned long long)(pow(10, 12)) == 38)
+        {
+                codOperadora = 4; //Dinnes
+        }
+        else if (numero / (unsigned long long)(pow(10, 12)) == 6011)
+        {
+                codOperadora = 5; //Discover
+        }
+        else if (numero / (unsigned long long)(pow(10, 11)) == 2014 || numero / (unsigned long long)(pow(10, 11)) == 2149)
+        {
+                codOperadora = 6; //enRoute
+        }
+        else if (numero / (unsigned long long)(pow(10, 15)) == 3)
+        {
+                codOperadora = 7; //JCB
+        }
+        else if (numero / (unsigned long long)(pow(10, 11)) == 2131 || numero / (unsigned long long)(pow(10, 11)) == 1800)
+        {
+                codOperadora = 8; //JCB
+        }
+        //SE X=0, O NUMERO � INVALIDO.*/
+        return *codOperadora != 0;
+}
+
+void numeroInvalido(unsigned long long numero, int codOperadora, int tamanhoN)
+{
+        int tipo = 0; // 1 = Maior; 2 = Menor
+        switch (codOperadora)
+        {
+        case 1:
+        case 5:
+        case 7:
+                tipo = (tamanhoN > 16) ? 1 : 2;
+                break;
+        case 3:
+        case 6:
+        case 8:
+                tipo = (tamanhoN > 15) ? 1 : 2;
+        case 4:
+                tipo = (tamanhoN > 14) ? 1 : 2;
+        case 2:
+                if (tamanhoN > 16)
+                        tipo = 1;
+                else if (tamanhoN < 13)
+                        tipo = 2;
+
+        }
+        if (tipo != 0) printf(((tipo == 1) ? "(Numero muito longo)\n" : "(Numero muito curto)\n"));
+        //printf("R: %d\n", numeroResultante); //TESTE
+        //return tamOk && (numeroResultante % 10 == 0);
+        /*if (((numero / (unsigned long long)(pow(10, 12))) < 10) || ((numero / (unsigned long long)(pow(10, 16))) > 10))
+        {
+                printf("Tamanho incorreto "); //CASO 2
+        }
+        else if (((numero / (unsigned long long)(pow(10, 12))) > 10) && ((numero / (unsigned long long)(pow(10, 16))) < 10))
+        {
+                printf("numero de operadoras desconhecidas  "); //CASO 3
+        }*/
+}
+/*void imprimirOperadora(int codOperadora)
+{
+        switch (codOperadora)
+        {
+        case 1:
+                printf("Mastercard\n");
+                break;
+        case 2:
+                printf("Visa\n");
+                break;
+        case 3:
+                printf("Amex\n");
+                break;
+        case 4:
+                printf("Diners\n");
+                break;
+        case 5:
+                printf("Discover\n");
+                break;
+        case 6:
+                printf("enRoute\n");
+                break;
+        case 7:
+                printf("JCB\n");
+                break;
+        case 8:
+                printf("JCB\n");
+                break;
+        }
+}*/
+
+int lerNroCartao(unsigned long long *numero, int *tamanho)
+{
+        int valido = 1;
+        char c;
+
+        printf("Digite o numero do Cartao: ");
+        scanf("%c", &c);
+
+        *tamanho = 0;
+        while (c != '\n')
+        {
+                //printf("C: %c %d",c,c);
+                if (c < 47 || c > 58)
+                {
+                        valido = 0;
+                        break;
+                }
+                *tamanho += 1;
+                *numero = *numero * 10 + (c - 48);
+                scanf("%c", &c);
+        }
+        printf("Nro.: %llu\n", *numero);
+        if (!valido || *tamanho > 16 || *tamanho < 13)
+        {
+                printf("Estado: \x1b[31m Invalido.");
+                if (!valido)
+                        printf(" (Caracteres invalidos encontrados).\x1b[0m\n");
+                else
+                        printf((*tamanho > 16) ? "(Nro. muito longo).\x1b[0m\n": "(Nro. muito curto).\x1b[0m\n");
+        }
+        return valido;
+}
+
+int validarNro(unsigned long long numero, int nroOperadora, int tamanhoN)
+{
+        int  tamOk = 1, numeroResultante = 0, num;
+        while (numero > 0) //Quantos algarismos tem o numero
+        {
+                num = numero % 10;
+                //printf("I: %d N: %d\n", tamanhoN, num); // TESTE
+                if (tamanhoN % 2 == 0)
+                {
+                        num = num * 2;
+                        if (num >= 10)
+                        {
+                                num = num % 10 + num / 10;
+                        }
+                }
+                numeroResultante += num;
+                numero /= 10;
+        }
+        //printf("Operadora: %d\n Tamanho: %d \n", nroOperadora, );
+        switch (nroOperadora)
+        {
+        case 1:
+        case 5:
+        case 7:
+                if (tamanhoN != 16) tamOk = 0;
+                break;
+        case 3:
+        case 6:
+        case 8:
+                if (tamanhoN != 15) tamOk = 0;
+        case 4:
+                if (tamanhoN != 14) tamOk = 0;
+        case 2:
+                if (tamanhoN != 16 && tamanhoN != 13) tamOk = 0;
+        }
+        //printf("R: %d\n", numeroResultante); //TESTE
+        return tamOk && (numeroResultante % 10 == 0);
 }
 
 unsigned long long restoLong(unsigned long long d1, unsigned long long d2)
 {
-    printf("Entrou em resto\n");
-    while (d1 >= d2)
-    {
-        d1 -= d2;
-    }
-    return d1;
+        printf("Entrou em resto\n");
+        while (d1 >= d2)
+        {
+                d1 -= d2;
+        }
+        return d1;
 }
-
